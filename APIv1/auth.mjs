065cd62b9bc0cba3399d195005e17531d70db777
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-09 12:52:13 +0500

    mongodb connected

--- APIv1/auth.mjs
+++ APIv1/auth.mjs
@@ -1,7 +1,14 @@
 import express from "express";
+import { client } from "../mongodb.mjs";
 const router = express.Router();
 
 router.post("/login", (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(403).send({ message: "Required Paramater Missing" });
+    return;
+  }
+  const emailInLower = req.body.email.toLowerCase();
+
   res.send("This is login v1 " + new Date());
 });
 router.post("/signup", (req, res, next) => {
